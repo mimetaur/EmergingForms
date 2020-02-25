01380edca95643a0065c28e84da92750373fc111
Nathan Koch <nathanrkoch@gmail.com>
2020-02-25 15:11:36 -0500

    intro scene exists. stop camera from animating initially

--- EmergingForms/Assets/Scripts/CameraController.cs
+++ EmergingForms/Assets/Scripts/CameraController.cs
@@ -7,7 +7,7 @@ public class CameraController : MonoBehaviour
     public Transform cameraOriginal;
     public Transform[] cameraDestinations;
 
-    public bool doTravelCamera = true;
+    public bool doTravelCamera = false;
     public float cameraChangeSpeed = 10.0f;
     public float smooth = 0.8f;
 
@@ -21,6 +21,7 @@ public class CameraController : MonoBehaviour
 
     void TravelCamera()
     {
+        if (!doTravelCamera) return;
         currentDestination = cameraDestinations[Random.Range(0, cameraDestinations.Length)];
         // 0 is delay / start
         // 1 is destination
@@ -32,6 +33,19 @@ public class CameraController : MonoBehaviour
     }
 
     void Update()
+    {
+        if (doTravelCamera)
+        {
+            LerpCameras();
+        }
+        else
+        {
+            travel = 0;
+        }
+
+    }
+
+    private void LerpCameras()
     {
         if (travel == 1 || travel == 3)
         {
